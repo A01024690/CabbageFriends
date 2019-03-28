eba13c4b99967f326d4fb7afa6e4a8b6bc7dd8ff
Eric Chang <eric.c.chang@outlook.com>
2019-03-28 16:37:36 -0700

    seller page stuff

---
+++ js/seller.js
@@ -0,0 +1,64 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCiAhAXTdVzymNXtsvvZGCuMSVj_lZWR-E",
+    authDomain: "projectx-cabbagefriends.firebaseapp.com",
+    databaseURL: "https://projectx-cabbagefriends.firebaseio.com",
+    projectId: "projectx-cabbagefriends",
+    storageBucket: "projectx-cabbagefriends.appspot.com",
+    messagingSenderId: "219900775267"
+  };
+  firebase.initializeApp(config);
+
+
+// var db = firebase.firestore();
+
+function writeData(farmName, location, price){
+// var farm = firebase.auth().farm.uid;
+	
+	firebase.database().ref('farms/' + farmName).set({
+		farmName: farmName,
+		location: location,
+		price: price
+	});
+}
+
+function storeData(){
+	var farmName = document.getElementById("fname").value;
+	var location = document.getElementById("flocation").value;
+	var price = document.getElementById("fprice").value;
+
+	
+
+	if(farmName != "" && location != "" && price !=""){
+		writeData(farmName, location, price);
+		document.getElementById("fname").value = "";
+		document.getElementById("flocation").value = "";
+		document.getElementById("fprice").value = "";
+	} else {
+		alert("please input all the blank!")
+	}
+}
+
+// function storeData(){
+// window.location='file:///C:/term%201/web/comp%201930/project%201/ProjectX/cabbage%20friend%20now.html';
+// }
+
+   
+        
+            
+    	
+
+
+
+
+
+
+
+// function storeData() {
+//   	db.collection("movies").doc("newhome").set({
+//     name: "newhime",
+//     date: "2010",
+//     desctiption: "jslkadsajdjsalkdjsalkjdlkjsadlkjsalkdj",
+//     actors: ["yoyoyo", "lalala"]
+//   });

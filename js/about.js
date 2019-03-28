c3628218ef3c6b557b1036ec89368b2a47b7f03c
victorwu <wuvic93@gmail.com>
2019-03-27 22:16:14 -0700

    layout update for eric

---
+++ js/about.js
@@ -0,0 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+$(document).ready(function(){
+  $("#aboutBtn").click(function(){
+    $("#title").fadeToggle("fast");
+  });
+});

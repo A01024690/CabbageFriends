eb3685c5d99545e096eceba03c0b07f679c79981
A01024690 <wgong@my.bcit.ca>
2019-04-06 19:21:47 -0700

    buy and reserve edited

--- js/number.js
+++ js/number.js
@@ -1,15 +1,12 @@
 (function( $ ) {
 
     $.fn.number = function(customOptions) {
-
         var options = {
-
             'containerClass' : 'number-style',
             'plus' : 'number-plus',
             'minus' : 'number-minus',
             'containerTag' : 'div',
             'btnTag' : 'span'
-
         };
 
         options = $.extend(true, options, customOptions);
@@ -43,80 +40,49 @@
         }
 
         if(+input.val() <= +min){
-
             minus.addClass('disabled');
-
         }
 
         if(+input.val() >= +max){
-
             plus.addClass('disabled');
-
         }
 
         minus.click(function () {
-
             var input = $(this).parent().find('input');
-
             var value = input.val();
             if (value == 0){
                 return;
             }
-
             if(+value > +min){
-
                 input.val(+value - step);
-
                 if(+input.val() === +min){
-
                     input.prev('.' + options.minus).addClass('disabled');
-
                 }
 
                 if(input.next('.' + options.plus).hasClass('disabled')){
-
                     input.next('.' + options.plus).removeClass('disabled')
-
                 }
-
             } else if(!min){
-
                 input.val(+value - step);
-
             }
-
         });
 
         plus.click(function () {
-
             var input = $(this).parent().find('input');
-
             var value = input.val();
-
             if(+value < +max){
-
                 input.val(+value + step);
-
                 if(+input.val() === +max){
-
                     input.next('.' + options.plus).addClass('disabled');
-
                 }
 
                 if(input.prev('.' + options.minus).hasClass('disabled')){
-
                     input.prev('.' + options.minus).removeClass('disabled')
-
                 }
 
             } else if(!max){
-
                 input.val(+value + step);
-
             }
-
         });
-
     };
-
 })(jQuery);
